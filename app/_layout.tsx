36779368643c6136aa8334705b1540d5bd19534b
AyeshaAbdulGhaffar0900 <ayesha.a.ghaffar07@gmail.com>
2025-07-11 00:45:50 +0500

    Updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,52 @@
-import { Stack } from "expo-router";
-import './global.css'
+// import { SplashScreen, Stack } from "expo-router";
+// import './global.css'
+// import { useFonts } from "expo-font";
+// import { useEffect } from "react";
+
+
+// export default function RootLayout() {
+//   const [fontsLoaded , error] = useFonts( map:{
+//     "Quicksand-Bold": require(id:'../assets/fonts/Quicksand-Bold.ttf'),
+//     "Quicksand-Light": require(id:"../assets/fonts/Quicksand-Light.ttf"),
+//     "Quicksand-Medium": require(id:"../assets/fonts/Quicksand-Medium.ttf"),
+//     "Quicksand-Regular": require(id:"../assets/fonts/Quicksand-Regular.ttf"),
+//     "Quicksand-SemiBold": require(id:"../assets/fonts/Quicksand-SemiBold.ttf"),
+
+
+//   });
+
+//   useEffect(effect:() =>{
+//     if (error) throw error;
+//     if(fontsLoaded) SplashScreen.hideAsync();
+//   },deps:[fontsLoaded,error])
+
+//   return <Stack 
+//   screenOptions={{ headerShown: false }}
+//   />;
+// }
+
+
+
+import { SplashScreen, Stack } from "expo-router";
+import './global.css';
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+    "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "Quicksand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "Quicksand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,37 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function Index() {
-  return (
-   <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-xl font-bold text-purple-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+ return(
+  <SafeAreaView>
+
+    <FlatList
+      data={offers}
+      renderItem={({ item , index }) => {
+        return(
+          <View>
+            <Pressable className="offer-card" style={{backgroundColor: item.color}}>
+             {({pressed}) => (
+              <Fragment>
+                <View className={"h-full w-1/2"}>
+                <Image
+                  source={item.image}
+                  // className={"size-full"}
+                  resizeMode="contain"
+                />
+                </View>
+              </Fragment>
+             )
+             }
+            </Pressable>
+          </View>
+        )
+      }
+      }
+      />
+  </SafeAreaView>
+ )
 }
+   
